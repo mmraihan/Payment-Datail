910b14b0897de4ae6762a9d1627e0c8f74447abd
mmraihan <mm_raihan@live.com>
2022-02-01 22:48:19 +0600

    Add Components, Service and Model.

---
+++ PaymentApp/src/app/shared/payment-detail/payment-detail.model.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-detail',
+  templateUrl: './payment-detail.model.html',
+  styleUrls: ['./payment-detail.model.css']
+})
+export class PaymentDetailModel implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
